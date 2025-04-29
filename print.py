a4846f7f2cfe3aa80a5fe900c45800c806689478
Muhammad Bachtiar Fadli <bachtiar.fadli98@gmail.com>
2025-04-29 21:30:51 +0700

    add print.py

---
+++ print.py
@@ -0,0 +1,14 @@
+print("Belajar Python Dasar")
+print("Pengenalan Python")
+print()
+print('========================= Kutip 1 =========================')
+print('Nama saya "Bachtiar", sedang belajar python')
+print('Nama saya \'Bachtiar\', sedang belajar python')
+print()
+print("========================= Kutip 2 =========================")
+print("Nama saya 'Bachtiar', sedang belajar python")
+print("Nama saya \"Bachtiar\", sedang belajar python")
+
+print()
+print("Tinggi saya:", 180, "Berat saya:", 80)
+
