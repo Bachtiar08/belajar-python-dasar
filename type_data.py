ca5dc805b99469cd8b1674db8293bcd3c50d48f1
Muhammad Bachtiar Fadli <bachtiar.fadli98@gmail.com>
2025-04-30 11:32:29 +0700

    .\type_data.py

---
+++ type_data.py
@@ -0,0 +1,66 @@
+print('Belajar Type Data')
+print()
+print('str/string : Teks')
+nama = 'Bachtiar'
+alamat = 'Pondok Pekayon Indah'
+print('nama',nama)
+print('alama',alamat)
+print()
+
+print('int / integer : Bilangan Bulan')
+umur = 26
+tinggi = 180
+berat_badan = 80
+print('umur:',umur)
+print('tinggi:',tinggi)
+print('berat badan:',berat_badan)
+print("Belajar Python Dasar")
+print("Pengenalan Python")
+print()
+print('========================= Kutip 1 =========================')
+print('Nama saya "Bachtiar", sedang belajar python')
+print('Nama saya \'Bachtiar\', sedang belajar python')
+print()
+print("========================= Kutip 2 =========================")
+print("Nama saya 'Bachtiar', sedang belajar python")
+print("Nama saya \"Bachtiar\", sedang belajar python")
+
+print()
+print("Tinggi saya:", 180, "Berat saya:", 80)
+
+
+print('========================= Belajar Type Data =========================')
+print()
+
+print('str / string : Teks')
+nama = 'Imbar Budiman' # Pendefinisian/assigment variabel 'nama'
+alamat = 'Cikarang, Kab. Bekasi'
+print('Nama:', nama)
+print('Alamat:', alamat)
+print('Type dari alamat:', type(alamat))
+print()
+
+print('int / integer : Bilangan Bulat')
+umur = 26
+tinggi = 180
+berat_badan = 80
+print('Umur:', umur)
+print('Tinggi:', tinggi)
+print('Berat Badan:', berat_badan)
+print('Type dari berat_badan:', type(berat_badan))
+berat_badan = "80"
+print('Type dari berat_badan berubah jadi:', type(berat_badan))
+print()
+
+print('Float: Bilangan Desimal')
+lingkar_perut = 20.5
+print('Lingkar Perut:', lingkar_perut)
+print('Type dari lingkar_perut:', type(lingkar_perut))
+print()
+
+print('Bool/Boolean: True atau False')
+tidur = False
+makan = True
+print('Sedang tidur:', tidur)
+print('Sedang makan:', makan)
+print('Type dari makan:', type(makan))
