08f885b0d1a90583ff11f0fea12ec9247c4a204d
Muhammad Bachtiar Fadli <bachtiar.fadli98@gmail.com>
2025-05-08 21:29:11 +0700

    operator_perbandingan

---
+++ operator_perbandingan.py
@@ -0,0 +1,52 @@
+print('========= Operator Perbandingan =========')
+print()
+ 
+print('# Sama Dengan (==)')
+a = 10 == 10
+print('a:', a)
+b = 10 == 20
+print('b:', b)
+print('3 == 3.1:', 3 == 3.1)
+print('10 == 11:', 10 == 11)
+print()
+ 
+print('# Tidak Sama Dengan (!=)')
+a = 10 != 10
+print('a:', a)
+b = 10 != 20
+print('b:', b)
+print('3 != 3.1:', 3 != 3.1)
+print('10 != 10:', 10 != 10)
+print()
+ 
+print('# Lebih Kecil (<)')
+a = 10 < 10
+print('a:', a)
+b = 1 < 5
+print('b:', b)
+print('9 < 10:', 9 < 10)
+print()
+ 
+print('# Lebih Besar (>)')
+a = 10 > 10
+print('a:', a)
+b = 5 > 1
+print('b:', b)
+print('10 > 9:', 10 > 9)
+print()
+ 
+print('# Lebih Kecil atau Sama Dengan (<=)')
+a = 10 <= 10
+print('a:', a)
+b = 5 <= 1
+print('b:', b)
+print('9 <= 10:', 9 <= 10)
+print()
+ 
+print('# Lebih Besar atau Sama Dengan (>=)')
+a = 10 >= 10
+print('a:', a)
+b = 1 >= 5
+print('b:', b)
+print('10 >= 9:', 10 >= 9)
+print()
