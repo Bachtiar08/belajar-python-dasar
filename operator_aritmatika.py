ee6eed6173e5f67a10625e1a0ff781e74825a725
Muhammad Bachtiar Fadli <bachtiar.fadli98@gmail.com>
2025-05-08 21:38:26 +0700

    tambah operator aritmatika

---
+++ operator_aritmatika.py
@@ -0,0 +1,64 @@
+print('========= Operator Aritmatika =========')
+print()
+ 
+print('# Penjumalahan (+)')
+a = 10 + 5
+print('a:', a)
+print('20 + 30:', 20 + 30)
+b = 10.2 + 5.3
+print('b:', b)
+print('20.2 + 30.3:', 20.2 + 30.3)
+print()
+ 
+print('# Pengurangan (-)')
+a = 10 - 5
+print('a:', a)
+print('20 - 10:', 20 - 10)
+b = 10.2 - 5.3
+print('b:', b)
+print('round(b):', round(b, 10))
+print('20.2 - 10.3:', 20.2 - 10.3)
+print()
+ 
+print('# Perkalian (*)')
+a = 10 * 5
+print('a:', a)
+print('20 * 2:', 20 * 2)
+b = 2.5 * 2.5
+print('b:', b)
+print('20.2 * 10.3:', 20.2 * 10.3)
+print()
+ 
+print('# Pembagian (/)')
+a = 10 / 5 # Hasil dari bagi integer akan menghasilkan float
+print('a:', a)
+print('20 / 2:', 20 / 2)
+b = 20.8 / 2.5
+print('b:', b)
+print('20.2 / 10.3:', 20.2 / 10.3)
+print()
+ 
+print('# Pembagian Dengan Pembulatan Floor/Kebawah (//)')
+a = 5 // 2
+print('a:', a)
+print('20 // 2:', 20 // 2)
+b = 20.8 // 2.5
+print('b:', b)
+print('type(b):', type(b))
+print()
+ 
+print('# Sisa Bagi / Modulus (%)')
+a = 5 % 2
+print('a:', a)
+print('20 % 3:', 20 % 3)
+b = 15 % 4
+print('b:', b)
+print()
+ 
+print('# Pangkat (**)')
+a = 5 ** 2
+print('a:', a)
+print('5 ** 3:', 5 ** 3)
+b = 10 ** 4
+print('b:', b)
+print()
